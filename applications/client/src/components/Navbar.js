75934e8d4f73212f23045cbfc48b4d3769caf492
snehalp31996 <snehalp31996@gmail.com>
2022-10-25 20:36:00 -0700

    added home, about, login and register page

---
+++ applications/client/src/components/Navbar.js
@@ -0,0 +1,44 @@
+
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import React, { Component } from 'react';
+import logo from "../assets/Home/logo192.png";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class NavbarComp extends Component {
+  render(){
+
+    // const handleLogout = () =>{
+    //   localStorage.removeItem("token");
+    //   window.location.reload();
+    // };
+
+
+    return (
+        <div>
+          <Navbar bg="dark" variant="dark" sticky="top" expand="lg" CollapseOnSelect>
+              <Navbar.Brand href="/"><img src={logo} alt="logo1" width="50px"/>Code Talkers</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll">
+                <Nav
+                  className="navbar-nav justify-content-start col-3 col-md-4"
+                  style={{ maxHeight: "100px" }}
+                  navbarScroll
+                >
+                  <Nav.Link href="/"> Home </Nav.Link>
+                  <Nav.Link href="https://beta.openai.com/docs/introduction"> OpenAI </Nav.Link>
+                  <Nav.Link href="/about"> About </Nav.Link>
+                  <Nav.Link href="/login"> Login </Nav.Link>
+                  {/* <button onClick={handleLogout}>Logout</button> */}
+                </Nav>
+                {/* <button className="btn highlighted-btn" onClick={handleLogout}>
+                  Logout
+                </button> */}
+              </Navbar.Collapse>
+          </Navbar>
+        </div>
+    );
+}
+}
+
+export default NavbarComp;
