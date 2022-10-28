94f9170f3d7685ab48c4b1ebec949e0a80e1bfbd
snehalp31996 <snehalp31996@gmail.com>
2022-10-27 18:11:37 -0700

    CSC890 added jwt token and cookie expiration on server

--- applications/server/models/user.js
+++ applications/server/models/user.js
@@ -1,34 +1,48 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const Joi = require('joi');
-const passwordComplexity = require('joi-password-complexity');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
 
 const userSchema = new mongoose.Schema({
-    firstName:{type:String, required:true},
-    lastName:{type:String, required:true},
-    email:{type:String, required:true},
-    password:{type:String, required:true},
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userType: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
 
-userSchema.methods.generateAuthToken = function(){
-    const token = jwt.sign({_id: this._id}, process.env.JWTPRIVATEKEY,{expiresIn: "7d"});
-    return token
-
+userSchema.methods.generateAuthToken = async function () {
+  try {
+    let token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY);
+    this.tokens = this.tokens.concat({ token: token });
+    await this.save();
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //user model
 const User = mongoose.model("user", userSchema);
 
+const validate = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string().required().label("First Name"),
+    lastName: Joi.string().required().label("Last Name"),
+    userType: Joi.string().required().label("User Type"),
+    email: Joi.string().email().required().label("Email"),
+    password: passwordComplexity().required().label("Password"),
+  });
 
-const validate = (data) =>{
-    const schema = Joi.object({
-        firstName:Joi.string().required().label("First Name"),
-        lastName:Joi.string().required().label("Last Name"),
-        email:Joi.string().email().required().label("Email"),
-        password:passwordComplexity().required().label("Password"),
-    });
+  return schema.validate(data);
+};
 
-    return schema.validate(data)
-}
-
-module.exports = {User, validate};
+module.exports = { User, validate };
